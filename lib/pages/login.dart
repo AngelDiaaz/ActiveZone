47d9d4f1cbf0754bb12a5c4606baf57c9c952ca7
adiaz <a_angel.diaz.aviles@iespablopicasso.es>
2023-03-24 18:56:34 +0100

    Conexion a bbdd

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -1,6 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import '../services/appstate.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
+// ...
+
+await Firebase.initializeApp(
+options: DefaultFirebaseOptions.currentPlatform,
+);
 
 class Login extends StatefulWidget {
   const Login({Key? key}) : super(key: key);
