96d66e82bc13720fb5fdaed52c5e408b488aaf33
adiaz <a_angel.diaz.aviles@iespablopicasso.es>
2023-03-29 17:32:35 +0200

    Obtencion de los usuarios de la bbdd

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -1,14 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
 import '../services/appstate.dart';
+import 'package:provider/provider.dart';
+
 import 'package:firebase_core/firebase_core.dart';
-import 'firebase_options.dart';
-
-// ...
-
-await Firebase.initializeApp(
-options: DefaultFirebaseOptions.currentPlatform,
-);
 
 class Login extends StatefulWidget {
   const Login({Key? key}) : super(key: key);
