4a70c26458b7ef808d4a0d38dae4f25180e7f5cb
adiaz <a_angel.diaz.aviles@iespablopicasso.es>
2023-04-07 13:10:16 +0200

    Mapeo firestore bien hecho

--- lib/models/user.dart
+++ lib/models/user.dart
@@ -1,3 +1,5 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
 class User {
   User({
     this.key,
@@ -18,6 +20,36 @@ class User {
   String surname1;
   String surname2;
   bool? active;
+
+  factory User.fromFirestore(
+    DocumentSnapshot<Map<String, dynamic>> snapshot,
+    SnapshotOptions? options,
+  ) {
+    final data = snapshot.data();
+    return User(
+      name: data?['name'],
+      password: data?['password'],
+      surname1: data?['surname1'],
+      surname2: data?['surname2'],
+      email: data?['email'],
+      authenticationCode: data?['authentication code'],
+      key: data?['key'],
+      active: data?['active'],
+    );
+  }
+
+  Map<String, dynamic> toFirestore() {
+    return {
+      "name": name,
+      "password": password,
+      "email": email,
+      "surname1": surname1,
+      "surname2": surname2,
+      if (active != null) "active": false,
+      if (authenticationCode != null) "authenticationCode": authenticationCode,
+      if (key != null) "key": key,
+    };
+  }
 
   @override
   String toString() {
--- lib/services/users_services.dart
+++ lib/services/users_services.dart
@@ -1,8 +1,4 @@
-import 'package:firebase_core/firebase_core.dart';
-import 'package:flutter/material.dart';
 import 'package:gymapp/models/models.dart';
-import 'firebase_options.dart';
-import '../models/user.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 class LoginServices {
@@ -48,32 +44,45 @@ class LoginServices {
   }
 
   Future<User> getUser(String id) async {
-    DocumentSnapshot snapshot = await db.collection("users").doc(id).get();
-    var data = snapshot.data() as Map;
-
-    print('hola');
-
-    //TODO arreglar obtener user2
-
-    if (snapshot.exists) {
-      // Obtengo el usuario de la bbdd
-      User user = User(
-        password: data.values.elementAt(1),
-        name: data.values.elementAt(3),
-        authenticationCode: data.values.elementAt(5),
-        email: data.values.elementAt(6),
-        key: data.values.elementAt(7),
-        surname1: data.values.elementAt(0),
-        surname2: data.values.elementAt(2),
-        active: data.values.elementAt(4),
-      );
-      print('a ' + user.toString());
-
+    final ref = db.collection("users").doc(id).withConverter(
+      fromFirestore: User.fromFirestore,
+      toFirestore: (User user, _) => user.toFirestore(),
+    );
+    
+    final docSnap = await ref.get();
+    User? user = docSnap.data(); // Convert to City object
+    if (user != null) {
       return user;
-
+    } else {
+      return userEmpty;
     }
-    return userEmpty;
   }
+
+  // Future<User> getUser(String id) async {
+  //   DocumentSnapshot snapshot = await db.collection("users").doc("user2").get();
+  //   var data = snapshot.data() as Map;
+  //
+  //   print('hola');
+  //
+  //
+  //   if (snapshot.exists) {
+  //     // Obtengo el usuario de la bbdd
+  //     User user = User(
+  //       password: data.values.elementAt(1),
+  //       name: data.values.elementAt(3),
+  //       // authenticationCode: data.values.elementAt(5),
+  //       email: data.values.elementAt(6),
+  //       key: data.values.elementAt(7),
+  //       surname1: data.values.elementAt(0),
+  //       surname2: data.values.elementAt(2),
+  //       // active: data.values.elementAt(4),
+  //     );
+  //     print('a ' + user.toString());
+  //
+  //     return user;
+  //   }
+  //   return userEmpty;
+  // }
     /// Metodo que modifica un usuario en la base de datos
     Future<bool> updateUser(String id, User user) async {
       try {
