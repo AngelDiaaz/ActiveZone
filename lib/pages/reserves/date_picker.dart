8e0dce08c7c671dbbf12b1b969937544e9b549a0
Ángel Díaz Avilés <93326288+AngelDiaaz@users.noreply.github.com>
2023-05-04 11:07:14 +0200

    Fechas en proceso

---
+++ lib/pages/reserves/date_picker.dart
@@ -0,0 +1,125 @@
+import 'package:flutter/material.dart';
+
+import '../../models/models.dart';
+import '../../services/appstate.dart';
+
+// void main() => runApp(const MyApp());
+//
+// class MyApp extends StatelessWidget {
+//   const MyApp({super.key});
+//
+//   static const String _title = 'Flutter Code Sample';
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return const MaterialApp(
+//       restorationScopeId: 'app',
+//       title: _title,
+//       home: DatePicker(restorationId: 'main'),
+//     );
+//   }
+// }
+
+class DatePicker extends StatefulWidget {
+  DatePicker({super.key, this.restorationId, this.gym, this.activity});
+
+  final String? restorationId;
+  static String? date;
+  final List<Schedule> s = [];
+  final Gym? gym;
+  final Activity? activity;
+
+  @override
+  State<DatePicker> createState() => _DatePickerState();
+}
+
+class _DatePickerState extends State<DatePicker> with RestorationMixin {
+  @override
+  String? get restorationId => widget.restorationId;
+  final RestorableDateTime _selectedDate = RestorableDateTime(DateTime.now());
+  late final RestorableRouteFuture<DateTime?> _restorableDatePickerRouteFuture =
+      RestorableRouteFuture<DateTime?>(
+    onComplete: _selectDate,
+    onPresent: (NavigatorState navigator, Object? arguments) {
+      return navigator.restorablePush(
+        _datePickerRoute,
+        arguments: _selectedDate.value.millisecondsSinceEpoch,
+      );
+    },
+  );
+  AppState appState = AppState();
+  String date = '11/05/2023';
+  List<Schedule> s = [];
+
+  static Route<DateTime> _datePickerRoute(
+    BuildContext context,
+    Object? arguments,
+  ) {
+    return DialogRoute<DateTime>(
+      context: context,
+      builder: (BuildContext context) {
+        return DatePickerDialog(
+          restorationId: 'date_picker_dialog',
+          initialEntryMode: DatePickerEntryMode.calendarOnly,
+          initialDate: DateTime.fromMillisecondsSinceEpoch(arguments! as int),
+          firstDate: DateTime.now(),
+          lastDate: DateTime(2023, 12, 31),
+        );
+      },
+    );
+  }
+
+  @override
+  void restoreState(RestorationBucket? oldBucket, bool initialRestore) {
+    registerForRestoration(_selectedDate, 'selected_date');
+    registerForRestoration(
+        _restorableDatePickerRouteFuture, 'date_picker_route_future');
+  }
+
+  void _selectDate(DateTime? newSelectedDate) {
+    if (newSelectedDate != null) {
+      setState(() {
+        _selectedDate.value = newSelectedDate;
+        ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+          content: Text(
+              'Selected: ${_selectedDate.value.day}/${_selectedDate.value.month}/${_selectedDate.value.year}'),
+        ));
+      });
+      signup();
+      date =
+          '${_selectedDate.value.day}/${_selectedDate.value.month}/${_selectedDate.value.year}';
+    }
+  }
+
+  Future<void> signup() async {
+    s = await appState.getShedulesByDate(date, widget.gym!.id, widget.activity!.name);
+    print(s);
+    print('object');
+    print(date);
+  }
+
+  List<Schedule> getS(){
+    return s;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: FutureBuilder(
+        future: signup(),
+        builder: (BuildContext context, AsyncSnapshot<dynamic> snapshot) {
+          return Center(
+            child: OutlinedButton(
+              onPressed: () async {
+                _restorableDatePickerRouteFuture.present();
+                s = await appState.getShedulesByDate(
+                    DatePicker.date!, 'dumbbell gym malaga', 'spinnig');
+              },
+              child: const Text('Seleccionar fecha'),
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
--- lib/pages/reserves/info_new_reserve.dart
+++ lib/pages/reserves/info_new_reserve.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:gymapp/pages/pages.dart';
 import 'package:gymapp/services/appstate.dart';
 import '../../models/models.dart';
+import 'info_hours.dart';
 
 /// Clase InfoNewReserve
 class InfoNewReserve extends StatefulWidget {
@@ -21,7 +22,6 @@ class _InfoNewReserveState extends State<InfoNewReserve> {
   int index = 0;
   Schedule schedule = Schedule(hour: '');
   AppState state = AppState();
-  String? choose = '11/05/2023';
   List<Schedule> a = [];
 
   @override
@@ -31,7 +31,7 @@ class _InfoNewReserveState extends State<InfoNewReserve> {
     width = widthScreen;
     var heightScreen = MediaQuery.of(context).size.height;
     var pages = [
-      infoHours(widthScreen, a, schedules),
+      InfoHours(activity: widget.activity,gym: widget.gym, user: widget.user),
       ConfirmReserve(
         schedule: schedule,
         activity: widget.activity,
@@ -68,134 +68,115 @@ class _InfoNewReserveState extends State<InfoNewReserve> {
       ),
     );
   }
-  Future<void> signup(String date) async {
-    a = await state.getShedulesByDate(date, widget.gym!.id, widget.activity.name);
-    print(a);
-    print(a.length);
-  }
 
-  Column infoHours(double widthScreen, List<Schedule> a, List<Schedule> schedule) {
-    var items = seeDate(schedule);
-    return Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: <Widget>[
-          Row(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              SizedBox(
-                height: 100,
-                width: widthScreen,
-                child: Center(
-                    child: Text(
-                  widget.activity.name,
-                  style: const TextStyle(
-                      fontSize: 30, fontWeight: FontWeight.bold),
-                )),
-              ),
-            ],
-          ),
-          const Divider(
-              height: 10, indent: 10, endIndent: 10, color: Colors.black54),
-          const SizedBox(
-            height: 20,
-          ),
-          Center(
-            child: Padding(
-              padding: const EdgeInsets.fromLTRB(10, 10, 10, 30),
-              child: DropdownButton(
-                value: choose,
-                style: const TextStyle(fontSize: 30, color: Colors.black),
-                icon: const Icon(Icons.keyboard_arrow_down),
-                items: items.map((String items) {
-                  return DropdownMenuItem(
-                    value: items,
-                    child: Text(items),
-                  );
-                }).toList(),
-                //TODO solucionar error al setState
-                onChanged: (String? newValue) {
-                  setState(() {
-                    choose = newValue!;
-                  });
-                  signup(choose!);
-                },
-              ),
-            ),
-          ),
-          //TODO mostrar divider bien arreglar y consulta fecha
-          const Divider(
-              height: 10, indent: 10, endIndent: 10, color: Colors.black54),
-          const SizedBox(
-            height: 20,
-          ),
-          printHours(a, widget.activity.capacity),
-        ]);
-  }
-
-  ///Metodo que devuelve una lista con las fechas de los horarios
-  List<String> seeDate(List<Schedule> schedules) {
-    List<String> dates = [];
-
-    for (Schedule s in schedules) {
-      if (!dates.contains(s.date)) dates.add(s.date!);
-    }
-    return dates;
-  }
-
-  /// Metodo que imprime todas las horas de una clase
-  Column printHours(List<Schedule> schedules, int activityCapacity) {
-    // Para saber cuantas filas hacen falta
-    var rows = schedules.length / 3;
-    var count = 0;
-    List<Schedule> s = state.getAvailableSchedules(schedules, widget.activity.capacity);
-    return Column(
-      children: [
-        for (int i = 0; i < rows; i++) ...[
-          Row(
-            children: [
-              for (int i = 0; i < 3; i++) ...[
-                if (count + 1 <= s.length) ...[
-                  Container(
-                    // Si no hay mas horarios imprime una columna vacia
-                    child: hourButton(s.elementAt(count++)),
-                  ),
-                ]
-              ],
-            ],
-          ),
-        ],
-      ],
-    );
-  }
-
-  /// Metodo que devuelve un boton con la hora de una clase
-  Container hourButton(Schedule schedule) {
-    return Container(
-      width: width / 3 - 30,
-      height: 65,
-      margin: const EdgeInsets.all(15.0),
-      padding: const EdgeInsets.all(2.0),
-      child: TextButton(
-        onPressed: () {
-          this.schedule = schedule;
-          setState(() {
-            index = 1;
-          });
-        },
-        style: ButtonStyle(
-            shape: MaterialStateProperty.all(RoundedRectangleBorder(
-                side: const BorderSide(
-                  color: Colors.black26, // your color here
-                  width: 1,
-                ),
-                borderRadius: BorderRadius.circular(2)))),
-        child: Text(
-          schedule.hour,
-          style: const TextStyle(
-              fontSize: 20, fontWeight: FontWeight.bold, color: Colors.black),
-        ),
-      ),
-    );
-  }
+  // Column infoHours(double widthScreen, List<Schedule> a, List<Schedule> schedule) {
+  //   var items = seeDate(schedule);
+  //   return Column(
+  //       crossAxisAlignment: CrossAxisAlignment.start,
+  //       children: <Widget>[
+  //         Row(
+  //           crossAxisAlignment: CrossAxisAlignment.center,
+  //           mainAxisAlignment: MainAxisAlignment.center,
+  //           children: [
+  //             SizedBox(
+  //               height: 100,
+  //               width: widthScreen,
+  //               child: Center(
+  //                   child: Text(
+  //                 widget.activity.name,
+  //                 style: const TextStyle(
+  //                     fontSize: 30, fontWeight: FontWeight.bold),
+  //               )),
+  //             ),
+  //           ],
+  //         ),
+  //         const Divider(
+  //             height: 10, indent: 10, endIndent: 10, color: Colors.black54),
+  //         const SizedBox(
+  //           height: 20,
+  //         ),
+  //        const Center(
+  //           child: SizedBox(
+  //               width: 200,
+  //               height: 100,
+  //               child: DatePicker()),
+  //         ),
+  //
+  //         //TODO mostrar divider bien arreglar y consulta fecha
+  //         const Divider(
+  //             height: 10, indent: 10, endIndent: 10, color: Colors.black54),
+  //         const SizedBox(
+  //           height: 20,
+  //         ),
+  //         printHours(a, widget.activity.capacity),
+  //       ]);
+  // }
+  //
+  // ///Metodo que devuelve una lista con las fechas de los horarios
+  // List<String> seeDate(List<Schedule> schedules) {
+  //   List<String> dates = [];
+  //
+  //   for (Schedule s in schedules) {
+  //     if (!dates.contains(s.date)) dates.add(s.date!);
+  //   }
+  //   print('hola');
+  //   print(DatePicker.date);
+  //   return dates;
+  // }
+  //
+  // /// Metodo que imprime todas las horas de una clase
+  // Column printHours(List<Schedule> schedules, int activityCapacity) {
+  //   // Para saber cuantas filas hacen falta
+  //   var rows = schedules.length / 3;
+  //   var count = 0;
+  //   List<Schedule> s = state.getAvailableSchedules(schedules, widget.activity.capacity);
+  //   return Column(
+  //     children: [
+  //       for (int i = 0; i < rows; i++) ...[
+  //         Row(
+  //           children: [
+  //             for (int i = 0; i < 3; i++) ...[
+  //               if (count + 1 <= s.length) ...[
+  //                 Container(
+  //                   // Si no hay mas horarios imprime una columna vacia
+  //                   child: hourButton(s.elementAt(count++)),
+  //                 ),
+  //               ]
+  //             ],
+  //           ],
+  //         ),
+  //       ],
+  //     ],
+  //   );
+  // }
+  //
+  // /// Metodo que devuelve un boton con la hora de una clase
+  // Container hourButton(Schedule schedule) {
+  //   return Container(
+  //     width: width / 3 - 30,
+  //     height: 65,
+  //     margin: const EdgeInsets.all(15.0),
+  //     padding: const EdgeInsets.all(2.0),
+  //     child: TextButton(
+  //       onPressed: () {
+  //         this.schedule = schedule;
+  //         setState(() {
+  //           index = 1;
+  //         });
+  //       },
+  //       style: ButtonStyle(
+  //           shape: MaterialStateProperty.all(RoundedRectangleBorder(
+  //               side: const BorderSide(
+  //                 color: Colors.black26, // your color here
+  //                 width: 1,
+  //               ),
+  //               borderRadius: BorderRadius.circular(2)))),
+  //       child: Text(
+  //         schedule.hour,
+  //         style: const TextStyle(
+  //             fontSize: 20, fontWeight: FontWeight.bold, color: Colors.black),
+  //       ),
+  //     ),
+  //   );
+  // }
 }
