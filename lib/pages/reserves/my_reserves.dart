f7415bbdc86c5c1b17176cbe0199697b2810ee0d
adiaz <a_angel.diaz.aviles@iespablopicasso.es>
2023-05-15 19:12:20 +0200

    Mis reservas en mejora

--- lib/pages/reserves/my_reserves.dart
+++ lib/pages/reserves/my_reserves.dart
@@ -86,38 +86,42 @@ class _MyReservesState extends State<MyReserves> {
                         future: loadList(),
                         builder: (BuildContext context,
                             AsyncSnapshot<void> snapshot) {
-                          if (snapshot.hasData) {
-                            //Si es la primera vez que se carga la pagina
-                            checkFirstTime();
+                          try {
+                            if (snapshot.hasData) {
+                              //Si es la primera vez que se carga la pagina
+                              checkFirstTime();
 
-                            return DropdownButton(
-                              value: dropdownValue,
-                              icon: const Icon(Icons.arrow_downward,
-                                  color: Colors.black),
-                              elevation: 16,
-                              alignment: Alignment.centerLeft,
-                              style: const TextStyle(
-                                  color: Colors.black,
-                                  fontWeight: FontWeight.bold,
-                                  fontSize: 30),
-                              iconSize: 34,
-                              onChanged: (String? value) {
-                                setState(() {
-                                  dropdownValue = value!;
-                                });
-                              },
-                              items: nameActivities
-                                  .map<DropdownMenuItem<String>>(
-                                      (String value) {
-                                return DropdownMenuItem<String>(
-                                  value: value,
-                                  child: Text(value),
-                                );
-                              }).toList(),
-                            );
-                          } else {
-                            return const Center(
-                                child: CircularProgressIndicator());
+                              return DropdownButton(
+                                value: dropdownValue,
+                                icon: const Icon(Icons.arrow_downward,
+                                    color: Colors.black),
+                                elevation: 16,
+                                alignment: Alignment.centerLeft,
+                                style: const TextStyle(
+                                    color: Colors.black,
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 30),
+                                iconSize: 34,
+                                onChanged: (String? value) {
+                                  setState(() {
+                                    dropdownValue = value!;
+                                  });
+                                },
+                                items: nameActivities
+                                    .map<DropdownMenuItem<String>>(
+                                        (String value) {
+                                  return DropdownMenuItem<String>(
+                                    value: value,
+                                    child: Text(value),
+                                  );
+                                }).toList(),
+                              );
+                            } else {
+                              return const Center(
+                                  child: CircularProgressIndicator());
+                            }
+                          } catch (e) {
+                            return Row();
                           }
                         }),
                     // const SizedBox(
@@ -137,38 +141,52 @@ class _MyReservesState extends State<MyReserves> {
                           future: loadList(),
                           builder: (BuildContext context,
                               AsyncSnapshot<void> snapshot) {
-                            if (snapshot.hasData) {
-                              checkFirstTime();
+                            try {
+                              if (snapshot.hasData) {
+                                checkFirstTime();
 
-                              return FutureBuilder<List<Schedule>>(
-                                  future: state.getSchedules(
-                                      'users', widget.user.dni, dropdownValue),
-                                  builder: (BuildContext context,
-                                      AsyncSnapshot<List<Schedule>> snapshot) {
-                                    if (snapshot.hasData) {
-                                      List<Schedule> schedules = snapshot.data!;
+                                return FutureBuilder<List<Schedule>>(
+                                    future: state.getSchedules('users',
+                                        widget.user.dni, dropdownValue),
+                                    builder: (BuildContext context,
+                                        AsyncSnapshot<List<Schedule>>
+                                            snapshot) {
+                                      try {
+                                        if (snapshot.hasData) {
+                                          List<Schedule> schedules =
+                                              snapshot.data!;
 
-                                      return ListView(
-                                        padding: const EdgeInsets.all(2),
-                                        children: [
-                                          for (int i = 0;
-                                              i < schedules.length;
-                                              i++)
-                                            reserveCard(
-                                                context,
-                                                widthScreen,
-                                                dropdownValue,
-                                                schedules.elementAt(i)),
-                                        ],
-                                      );
-                                    } else {
-                                      return const Center(
-                                          child: CircularProgressIndicator());
-                                    }
-                                  });
-                            } else {
-                              return const Center(
-                                  child: CircularProgressIndicator());
+                                          return ListView(
+                                            padding: const EdgeInsets.all(2),
+                                            children: [
+                                              for (int i = 0;
+                                                  i < schedules.length;
+                                                  i++)
+                                                reserveCard(
+                                                    context,
+                                                    widthScreen,
+                                                    dropdownValue,
+                                                    schedules.elementAt(i)),
+                                            ],
+                                          );
+                                        } else {
+                                          return const Center(
+                                              child:
+                                                  CircularProgressIndicator());
+                                        }
+                                      } catch (e) {
+                                        return Row();
+                                      }
+                                    });
+                              } else {
+                                return const Center(
+                                    child: CircularProgressIndicator());
+                              }
+                            } catch (e) {
+                              return const AlertDialog(
+                                title: Text('Lo sentimos no dispones de ninguna reserva pendiente',textAlign: TextAlign.center, style: TextStyle(wordSpacing: 2)),
+                                icon: Icon(Icons.sentiment_dissatisfied_outlined, color:  Colors.redAccent,size: 50),
+                              );
                             }
                           }),
                     ),
@@ -196,72 +214,76 @@ class _MyReservesState extends State<MyReserves> {
     return FutureBuilder(
       future: state.getImageActivity(name),
       builder: (BuildContext context, AsyncSnapshot<String> snapshot) {
-        if (snapshot.hasData) {
-          String image = snapshot.data!;
+        try {
+          if (snapshot.hasData) {
+            String image = snapshot.data!;
 
-          return Card(
-              shape: RoundedRectangleBorder(
-                side: BorderSide(
-                  color: Theme.of(context).colorScheme.outline,
+            return Card(
+                shape: RoundedRectangleBorder(
+                  side: BorderSide(
+                    color: Theme.of(context).colorScheme.outline,
+                  ),
                 ),
-              ),
-              elevation: 4,
-              margin: const EdgeInsets.symmetric(vertical: 4, horizontal: 6),
-              child: SizedBox(
-                width: widthScreen - 12,
-                height: 150,
-                child: Column(
-                  children: [
-                    Container(
-                        alignment: Alignment.centerLeft,
-                        padding: const EdgeInsets.fromLTRB(10, 20, 0, 0),
-                        child: Row(children: [
-                          SizedBox(
-                            width: 100,
-                            height: 100,
-                            child: Image.network(image, fit: BoxFit.cover),
-                          ),
-                          SizedBox(
-                            width: widthScreen / 2 - 130,
-                          ),
-                          Column(
-                            children: [
-                              Text(
-                                name,
-                                style: const TextStyle(
-                                    fontSize: 24,
-                                    color: Colors.black,
-                                    fontWeight: FontWeight.bold),
-                              ),
-                              const SizedBox(
-                                height: 10,
-                              ),
-                              Text(
-                                DateFormat('dd/MM/yyyy')
-                                    .format(schedule.date.toDate()),
-                                style: const TextStyle(
-                                    fontSize: 24,
-                                    color: Colors.black,
-                                    fontWeight: FontWeight.bold),
-                              ),
-                              const SizedBox(
-                                height: 10,
-                              ),
-                              Text(
-                                schedule.hour,
-                                style: const TextStyle(
-                                    fontSize: 24,
-                                    color: Colors.black,
-                                    fontWeight: FontWeight.bold),
-                              ),
-                            ],
-                          ),
-                        ])),
-                  ],
-                ),
-              ));
-        } else {
-          return const Center(child: CircularProgressIndicator());
+                elevation: 4,
+                margin: const EdgeInsets.symmetric(vertical: 4, horizontal: 6),
+                child: SizedBox(
+                  width: widthScreen - 12,
+                  height: 150,
+                  child: Column(
+                    children: [
+                      Container(
+                          alignment: Alignment.centerLeft,
+                          padding: const EdgeInsets.fromLTRB(10, 20, 0, 0),
+                          child: Row(children: [
+                            SizedBox(
+                              width: 100,
+                              height: 100,
+                              child: Image.network(image, fit: BoxFit.cover),
+                            ),
+                            SizedBox(
+                              width: widthScreen / 2 - 130,
+                            ),
+                            Column(
+                              children: [
+                                Text(
+                                  name,
+                                  style: const TextStyle(
+                                      fontSize: 24,
+                                      color: Colors.black,
+                                      fontWeight: FontWeight.bold),
+                                ),
+                                const SizedBox(
+                                  height: 10,
+                                ),
+                                Text(
+                                  DateFormat('dd/MM/yyyy')
+                                      .format(schedule.date.toDate()),
+                                  style: const TextStyle(
+                                      fontSize: 24,
+                                      color: Colors.black,
+                                      fontWeight: FontWeight.bold),
+                                ),
+                                const SizedBox(
+                                  height: 10,
+                                ),
+                                Text(
+                                  schedule.hour,
+                                  style: const TextStyle(
+                                      fontSize: 24,
+                                      color: Colors.black,
+                                      fontWeight: FontWeight.bold),
+                                ),
+                              ],
+                            ),
+                          ])),
+                    ],
+                  ),
+                ));
+          } else {
+            return const Center(child: CircularProgressIndicator());
+          }
+        } catch (e) {
+          return Row();
         }
       },
     );
